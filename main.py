ef24b0ebb50c01e4c3a45eb604ba9e77a9406223
Abduraimov Muminjon <muminjonguru@outlook.com>
2023-11-30 11:29:55 +0000

    finalized

--- main.py
+++ main.py
@@ -34,13 +34,90 @@ def calculate_vat(amount, country_code):
         raise
 
 def generate_html_invoice(items, customer_info, subtotal, total_vat):
-  pass
+    """
+    Generates the HTML content for the invoice.
+    """
+    items_html = "".join(
+        f"<tr><td>{item['description']}</td><td>{item['quantity']}</td><td>${item['unit_price']}</td></tr>"
+        for item in items
+    )
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Invoice</title>
+        <!-- Styles here -->
+    </head>
+    <body>
+        <h1>Invoice #{customer_info['invoice_number']}</h1>
+        <p>Date: {customer_info['date']}</p>
+        <!-- Customer details here -->
+        <h2>Invoice Details</h2>
+        <table>
+            <tr>
+                <th>Description</th>
+                <th>Quantity</th>
+                <th>Unit Price</th>
+            </tr>
+            {items_html}
+        </table>
+        <h3>Summary</h3>
+        <p>Subtotal: ${subtotal}</p>
+        <p>VAT: ${total_vat}</p>
+        <p>Total: ${subtotal + total_vat}</p>
+    </body>
+    </html>
+    """
+    return html_content
 
 def convert_html_to_pdf(html_content, output_filename='invoice.pdf'):
-  pass    
+    """
+    Converts the given HTML content to a PDF using the PDFLayer API.
+    """
+    url = "http://api.pdflayer.com/api/convert"
+    params = {
+        'access_key': PDFLAYER_API_KEY,
+    }
+    data = {
+        'document_html': html_content,
+        'page_size': 'A4'
+    }
+    try:
+        response = requests.post(url, params=params, data=data)
+        response.raise_for_status()
+
+        if response.headers.get('Content-Type', '').lower() == 'application/pdf':
+            with open(output_filename, 'wb') as f:
+                f.write(response.content)
+        else:
+            print("Error: Received non-PDF response.")
+            print(response.content.decode('utf-8'))  # For debugging
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+        raise
 
 def update_and_convert_html_to_pdf(items, customer_info, country_code, output_filename='invoice.pdf'):
-  pass
+    """
+    Updates the invoice HTML with given details, calculates VAT, and converts it to a PDF.
+    """
+    subtotal = sum(item['quantity'] * item['unit_price'] for item in items)
+    total_amount = calculate_vat(subtotal, country_code)
+    total_vat = total_amount - subtotal
 
+    html_content = generate_html_invoice(items, customer_info, subtotal, total_vat)
+    convert_html_to_pdf(html_content, output_filename)
 
-# update_and_convert_html_to_pdf is the final function
+# Example usage
+items = [
+    {"description": "Product 1", "quantity": 2, "unit_price": 50},
+    {"description": "Service 1", "quantity": 1, "unit_price": 80}
+]
+
+customer_info = {
+    "invoice_number": "12345",
+    "date": "2023-11-29",
+    "name": "John Doe",
+    "address": "1234 Street, City, Country"
+}
+
+update_and_convert_html_to_pdf(items, customer_info, 'GB')
